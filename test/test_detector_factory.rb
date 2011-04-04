10456fcb9ca94b206b8c95b65b836d24df4bea08
Jan Szumiec <jan.szumiec@gmail.com>
2011-04-04 20:11:00 +0100

    Added tests for DetectorFactory.

--- test/test_detector_factory.rb
+++ test/test_detector_factory.rb
@@ -1,5 +1,16 @@
 require 'test/helper'
 
 class DetectorFactoryTest < Test::Unit::TestCase
-  
+  def test_add_profile
+    profile = LangProfile.new
+    factory = DetectorFactory.new
+
+    factory.add_profile(profile, 0, 1)
+    assert_raises(LangDetectException) do
+      factory.add_profile(profile, 1, 1)
+    end
+    
+    detector = factory.create(0.123)
+    assert_equal(0.123, detector.alpha)
+  end
 end
