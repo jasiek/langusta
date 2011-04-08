74e1d34046a89c5aab9f6d1f37f2e46fcd5e32f1
Jan Szumiec <jan.szumiec@gmail.com>
2011-04-08 23:00:00 +0200

    Added accuracy tests.

--- Rakefile
+++ Rakefile
@@ -25,14 +25,14 @@ Jeweler::RubygemsDotOrgTasks.new
 require 'rake/testtask'
 Rake::TestTask.new(:test) do |test|
   test.libs << 'lib' << 'test'
-  test.pattern = 'test/**/test_*.rb'
+  test.pattern = 'test/test_*.rb'
   test.verbose = true
 end
 
 require 'rcov/rcovtask'
 Rcov::RcovTask.new do |test|
   test.libs << 'test'
-  test.pattern = 'test/**/test_*.rb'
+  test.pattern = 'test/test_*.rb'
   test.verbose = true
 end
 
@@ -46,4 +46,10 @@ Rake::RDocTask.new do |rdoc|
   rdoc.title = "langusta #{version}"
   rdoc.rdoc_files.include('README*')
   rdoc.rdoc_files.include('lib/**/*.rb')
+end
+
+Rake::TestTask.new('test:quality') do |test|
+  test.libs << 'test/quality'
+  test.pattern = 'test/quality/test_*.rb'
+  test.verbose = true
 end
---
+++ test/quality/test_falsified.rb
@@ -0,0 +1,33 @@
+require 'test/helper'
+
+class FalsifiedTest < Test::Unit::TestCase
+  ITERATIONS = 10
+
+  def test_incorrect_guesses
+    factory = DetectorFactory.new
+    profiles = Dir[File.join(PROFILES_PATH, '*')].map do |filename|
+      LangProfile.load_from_file(filename)
+    end
+    profiles.each_with_index do |profile, index|
+      factory.add_profile(profile, index, profiles.length)
+    end
+
+    incorrect_guesses = 0.0
+
+    ITERATIONS.times do
+      Dir['test/test_data/*'].each do |filename|
+        language = filename.split(/\//).last
+        
+        ucs2_content = UCS2String.from_utf8(File.open(filename).read)
+        detector = factory.create
+        detector.append(ucs2_content)
+        
+        incorrect_guesses += 1 if language != detector.detect
+      end
+    end
+
+    puts
+    puts "Accuracy: %s%%" % [100.0 - incorrect_guesses / Dir['test/test_data/*'].length / ITERATIONS]
+    puts
+  end
+end
