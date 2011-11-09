d8d572881a8717c59c8c61e4eeb5f54e3bdc13f0
Jan Szumiec <jan.szumiec@gmail.com>
2011-11-09 11:30:09 +0100

    Removed unused methods. Langusta is now compatible with 1.9.

--- test/test_ucs2_string.rb
+++ test/test_ucs2_string.rb
@@ -18,11 +18,6 @@ class UCS2StringTest < Test::Unit::TestCase
     end
   end
 
-  def test_convert_to_ucs2_codepoints
-    assert_equal([97], UCS2String::Converter.to_ucs2_codepoints('a'))
-    assert_equal([122, 97, 380, 243, 322, 263, 32, 103, 281, 347, 108, 261, 32, 106, 97, 378, 324], UCS2String::Converter.to_ucs2_codepoints('zażółć gęślą jaźń'))
-  end
-
   def test_convert_to_ucs2_string
     assert_equal("\x00a", UCS2String::Converter.to_ucs2_string('a'))
     assert_equal("\x00z\x00a\x01\x7c\x00\xf3\x01\x42\x01\x07\x00\x20\x00\x67\x01\x19\x01\x5b\x00\x6c\x01\x05\x00\x20\x00\x6a\x00\x61\x01\x7a\x01\x44", UCS2String::Converter.to_ucs2_string('zażółć gęślą jaźń'))
