7c8de7c0bbe5deca1627072e0cda40043aacd89c
Jan Szumiec <jan.szumiec@gmail.com>
2012-01-10 01:05:23 +0100

    UnicodeBlock now operates on codepoints.

--- lib/langusta/unicode_block.rb
+++ lib/langusta/unicode_block.rb
@@ -2,44 +2,44 @@ module Langusta
   module UnicodeBlock
     # Half-baked implementation of Java's UnicodeBlock.
 
-    OTHER = 0
-    BASIC_LATIN = 1
-    LATIN_1_SUPPLEMENT = 2
-    GENERAL_PUNCTUATION = 3
-    ARABIC = 4
-    LATIN_EXTENDED_ADDITIONAL = 5
-    HIRAGANA = 6
-    KATAKANA = 7
-    BOPOMOFO = 8
-    BOPOMOFO_EXTENDED = 9
-    CJK_UNIFIED_IDEOGRAPHS = 10
-    HANGUL_SYLLABES = 11
+    OTHER                           = 0
+    BASIC_LATIN                     = 1
+    LATIN_1_SUPPLEMENT              = 2
+    GENERAL_PUNCTUATION             = 3
+    ARABIC                          = 4
+    LATIN_EXTENDED_ADDITIONAL       = 5
+    HIRAGANA                        = 6
+    KATAKANA                        = 7
+    BOPOMOFO                        = 8
+    BOPOMOFO_EXTENDED               = 9
+    CJK_UNIFIED_IDEOGRAPHS          = 10
+    HANGUL_SYLLABES                 = 11
 
-    BASIC_LATIN_RANGE = "\x00\x00".."\x00\x7f"
-    LATIN_1_SUPPLEMENT_RANGE = "\x00\x80".."\x00\xff"
-    GENERAL_PUNCTUATION_RANGE = "\x20\x00".."\x20\x6f"
-    ARABIC_RANGE = "\x06\x00".."\x06\xff"
-    LATIN_EXTENDED_ADDITIONAL_RANGE = "\x1e\x00".."\x1e\xff"
-    HIRAGANA_RANGE = "\x30\x40".."\x30\x9f"
-    KATAKANA_RANGE = "\x30\xa0".."\x30\xff"
-    BOPOMOFO_RANGE = "\x31\x00".."\x31\xbf"
-    BOPOMOFO_EXTENDED_RANGE = "\x31\xa0".."\x31\xbf"
-    CJK_UNIFIED_IDEOGRAPHS_RANGE = "\x4e\x00".."\x9f\xff"
-    HANGUL_SYLLABES_RANGE = "\xac\x00".."\xd7\xaf"
+    BASIC_LATIN_RANGE               = 0x0000..0x007f
+    LATIN_1_SUPPLEMENT_RANGE        = 0x0080..0x00ff
+    GENERAL_PUNCTUATION_RANGE       = 0x2000..0x206f
+    ARABIC_RANGE                    = 0x0600..0x06ff
+    LATIN_EXTENDED_ADDITIONAL_RANGE = 0x1e00..0x1eff
+    HIRAGANA_RANGE                  = 0x3040..0x309f
+    KATAKANA_RANGE                  = 0x30a0..0x30ff
+    BOPOMOFO_RANGE                  = 0x3100..0x31bf
+    BOPOMOFO_EXTENDED_RANGE         = 0x31a0..0x31bf
+    CJK_UNIFIED_IDEOGRAPHS_RANGE    = 0x4e00..0x9fff
+    HANGUL_SYLLABES_RANGE           = 0xac00..0xd7af
 
     def self.of(character)
       case character
-      when BASIC_LATIN_RANGE then return BASIC_LATIN
-      when LATIN_1_SUPPLEMENT_RANGE then return LATIN_1_SUPPLEMENT
-      when GENERAL_PUNCTUATION_RANGE then return GENERAL_PUNCTUATION
-      when ARABIC_RANGE then return ARABIC
+      when BASIC_LATIN_RANGE               then return BASIC_LATIN
+      when LATIN_1_SUPPLEMENT_RANGE        then return LATIN_1_SUPPLEMENT
+      when GENERAL_PUNCTUATION_RANGE       then return GENERAL_PUNCTUATION
+      when ARABIC_RANGE                    then return ARABIC
       when LATIN_EXTENDED_ADDITIONAL_RANGE then return LATIN_EXTENDED_ADDITIONAL
-      when HIRAGANA_RANGE then return HIRAGANA
-      when KATAKANA_RANGE then return KATAKANA
-      when BOPOMOFO_RANGE then return BOPOMOFO
-      when BOPOMOFO_EXTENDED_RANGE then return BOPOMOFO_EXTENDED
-      when CJK_UNIFIED_IDEOGRAPHS_RANGE then return CJK_UNIFIED_IDEOGRAPHS
-      when HANGUL_SYLLABES_RANGE then return HANGUL_SYLLABES
+      when HIRAGANA_RANGE                  then return HIRAGANA
+      when KATAKANA_RANGE                  then return KATAKANA
+      when BOPOMOFO_RANGE                  then return BOPOMOFO
+      when BOPOMOFO_EXTENDED_RANGE         then return BOPOMOFO_EXTENDED
+      when CJK_UNIFIED_IDEOGRAPHS_RANGE    then return CJK_UNIFIED_IDEOGRAPHS
+      when HANGUL_SYLLABES_RANGE           then return HANGUL_SYLLABES
       else
         return OTHER
       end
@@ -50,7 +50,7 @@ module Langusta
     end
 
     def self.compute_upper_case_table
-      File.open(UPPERCASE_BIN).read
+      File.open(UPPERCASE_BIN).read.unpack('n*')
     end
   end
 end
--- test/test_unicode_block.rb
+++ test/test_unicode_block.rb
@@ -3,7 +3,7 @@ require 'test/helper'
 
 class UnicodeBlockTest < Test::Unit::TestCase
   def test_upper_case
-    ["\x00\x47", "\x01\x10", "\x01\x64", "\x03\xd5", "\x04\xa2", "\x10\xc3", "\x21\x60", "\xa7\x60"].each do |cp|
+    [0x0047, 0x0110, 0x0164, 0x03d5, 0x04a2, 0x10c3, 0x2160, 0xa760].each do |cp|
       assert(Langusta::UnicodeBlock.is_upper_case?(cp))
     end
   end
